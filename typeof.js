013cc2db8c73a872f56870faf2cf793850d59927
phpwechat <3257132998@qq.com>
2022-05-06 23:11:32 +0800

    format typeof.js

--- typeof.js
+++ typeof.js
@@ -7,10 +7,10 @@
      */
 
     'Array Boolean Date Function NaN Number Integer Object RegExp String Undefined Null'.split(' ').forEach(function (type) {
-      r['is' + type] = function (v) {
-        return v !== v ? type === 'NaN' ? !0 : !1 : v ===null && type==='Null' ? true: v % 1 === 0 && typeof v ==='number' && type==='Integer' ? true:RegExp(type).test(Object.prototype.toString.call(v));
-      };
+        r['is' + type] = function (v) {
+            return v !== v ? type === 'NaN' ? !0 : !1 : v === null && type === 'Null' ? true : v % 1 === 0 && typeof v === 'number' && type === 'Integer' ? true : RegExp(type).test(Object.prototype.toString.call(v));
+        };
     });
-  
+
     window.$$ = r;
-  }([]);
+}([]);
